4fe2ed8d1a1f3ca9c66e1b1195e4fb47fbf5b4eb
alirezamou <am808036@gmail.com>
2023-03-04 21:28:44 +0330

    add AppLayout

---
+++ src/layouts/AppLayout.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="w-screen h-screen flex">
+    <slot name="sidebar">Fallback Side bar</slot>
+    <slot name="mainContent" class="flex-grow">Main Content Fallback</slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppLayout",
+};
+</script>

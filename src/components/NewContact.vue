26d7e1b904434af54ec8a1ed09ea563c36d194a2
alirezamou <am808036@gmail.com>
2023-03-05 23:02:18 +0330

    add NewContact and ShowContact comp

---
+++ src/components/NewContact.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1 class="font-bold text-xl">Create New Contact</h1>
+    <ContactForm
+      submitButtonLabel="Create"
+      @submitContact="(e) => addContact(e)"
+    />
+  </div>
+</template>
+
+<script>
+import ContactForm from "./ContactForm";
+
+export default {
+  name: "NewContact",
+  components: {
+    ContactForm,
+  },
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {
+    addContact(payload) {
+      this.$emitter.emit("add-contact", payload);
+    },
+  },
+};
+</script>
---
+++ src/components/ShowContact.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Show Contact</h1>
+    {{ contactId }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ShowContact",
+  data() {
+    return {
+      contactId: this.$route.params.contactId,
+    };
+  },
+  computed: {},
+  methods: {
+    submit(data) {
+      console.log(data);
+    },
+  },
+  watch: {
+    $route: {
+      handler() {
+        this.contactId = this.$route.params.contactId;
+      },
+    },
+  },
+};
+</script>

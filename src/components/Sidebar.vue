b8c273f629720d66eaf444308a0dfe034f8b5f88
alirezamou <am808036@gmail.com>
2023-03-09 23:00:52 +0330

    fix style of wrapper 'div' around 'LoadingOverlay'

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -5,7 +5,7 @@
       <router-link to="/" class="!text-[#000] hover:no-underline hover:text-[#000]">+ new contact</router-link>
     </div>
     <input type="text" placeholder="Search Name" class="input" v-model="searchQuery">
-    <div v-if="isFetching" class="h-full flex flex-col justify-center items-center">
+    <div v-if="isFetching" class="h-full flex flex-col justify-start items-center">
       <LoadingOverlay classnames="text-purple"/>
     </div>
     <ListComp v-else :items="filteredContacts" itemKey="name" type="contact" />
